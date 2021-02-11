a5ea266db80a8a40f3b57f12cdcce7cca3f832a7
fruitdude <stevenrogge95@gmail.com>
2021-02-11 19:55:40 +0100

    players can score goals + ui integration

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -1,10 +1,29 @@
 extends Spatial
 
 
+var player1_score:= 0
+var player2_score:= 0
+
+
 func _on_GoalDetector_body_entered(body, goal_id):
 	get_tree().call_group("Actors", "freeze")
 	$Timer.start()
+	update_score(goal_id)
 
 
 func _on_Timer_timeout():
 	get_tree().call_group("Actors", "reset")
+
+
+func update_score(goal_id):
+	var new_score
+	
+	match goal_id:
+		1:
+			player1_score += 1	
+			new_score = player1_score
+		2:
+			player2_score += 1
+			new_score = player2_score
+			
+	$GUI.update_score(goal_id, new_score)
